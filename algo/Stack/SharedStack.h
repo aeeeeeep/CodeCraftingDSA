d10698401ad0c1176f1f341d9563877b6fbb1ba4
Aeeeeeep <aep_sun@qq.com>
2023-07-04 22:41:36 +0800

    add Stack

---
+++ algo/Stack/SharedStack.h
@@ -0,0 +1,103 @@
+#include <iostream>
+#include "SqStack.h"
+
+using namespace std;
+
+template <typename T>
+class SharedStack {
+private:
+    static const int MAX_SIZE = 100;  // 栈的最大容量
+    T data[MAX_SIZE];  // 栈的存储空间
+    int top[2];        // 两个栈的栈顶指针
+
+public:
+    // 构造函数
+    SharedStack() {
+        top[0] = -1;
+        top[1] = MAX_SIZE;
+    }
+
+    // 析构函数
+    ~SharedStack() {}
+
+    // 入栈操作
+    void Push(int stackNumber, const T& e) {
+        if (stackNumber != 0 && stackNumber != 1) {
+            cout << "无效的栈编号！" << endl;
+            return;
+        }
+
+        if (top[0] + 1 == top[1]) {
+            cout << "栈已满，无法执行入栈操作！" << endl;
+            return;
+        }
+
+        if (stackNumber == 0) {
+            top[0]++;
+            data[top[0]] = e;
+        } else {
+            top[1]--;
+            data[top[1]] = e;
+        }
+    }
+
+    // 出栈操作
+    void Pop(int stackNumber, T& e) {
+        if (stackNumber != 0 && stackNumber != 1) {
+            cout << "无效的栈编号！" << endl;
+            return;
+        }
+
+        if (stackNumber == 0) {
+            if (top[0] == -1) {
+                cout << "栈" << stackNumber << "为空，无法执行出栈操作！" << endl;
+                return;
+            }
+            e = data[top[0]];
+            top[0]--;
+        } else {
+            if (top[1] == MAX_SIZE) {
+                cout << "栈" << stackNumber << "为空，无法执行出栈操作！" << endl;
+                return;
+            }
+            e = data[top[1]];
+            top[1]++;
+        }
+    }
+
+    // 读栈顶操作
+    T GetTop(int stackNumber) {
+        if (stackNumber != 0 && stackNumber != 1) {
+            cout << "无效的栈编号！" << endl;
+            return -1;
+        }
+
+        if (stackNumber == 0) {
+            if (top[0] == -1) {
+                cout << "栈" << stackNumber << "为空，无法读取栈顶元素！" << endl;
+                return -1;
+            }
+            return data[top[0]];
+        } else {
+            if (top[1] == MAX_SIZE) {
+                cout << "栈" << stackNumber << "为空，无法读取栈顶元素！" << endl;
+                return -1;
+            }
+            return data[top[1]];
+        }
+    }
+
+    // 判空操作
+    bool Empty(int stackNumber) {
+        if (stackNumber != 0 && stackNumber != 1) {
+            cout << "无效的栈编号！" << endl;
+            return false;
+        }
+
+        if (stackNumber == 0) {
+            return (top[0] == -1);
+        } else {
+            return (top[1] == MAX_SIZE);
+        }
+    }
+};
--- algo/Stack/main.cpp
+++ algo/Stack/main.cpp
@@ -1,28 +1,29 @@
-#include "LinkStack.h"
+#include "SharedStack.h"
 
 int main() {
-    LinkStack<int> stack;
-    stack.Push(1);
-    stack.Push(2);
-    stack.Push(3);
+    SharedStack<int> stack;
 
-    int topElement = stack.GetTop();
-    cout << "栈顶元素为: " << topElement << endl;
+    // 入栈操作
+    stack.Push(0, 1);  // 向栈0入栈元素1
+    stack.Push(1, 2);  // 向栈1入栈元素2
+    stack.Push(0, 3);  // 向栈0入栈元素3
+    stack.Push(1, 4);  // 向栈1入栈元素4
 
+    // 读取栈顶元素
+    cout << "栈0的栈顶元素: " << stack.GetTop(0) << endl;
+    cout << "栈1的栈顶元素: " << stack.GetTop(1) << endl;
+
+    // 出栈操作
     int poppedElement;
-    stack.Pop(poppedElement);
-    cout << "出栈元素为: " << poppedElement << endl;
-    stack.Pop(poppedElement);
-    cout << "出栈元素为: " << poppedElement << endl;
-    stack.Pop(poppedElement);
-    cout << "出栈元素为: " << poppedElement << endl;
+    stack.Pop(0, poppedElement);  // 从栈0出栈
+    cout << "从栈0出栈的元素: " << poppedElement << endl;
+    stack.Pop(1, poppedElement);  // 从栈1出栈
+    cout << "从栈1出栈的元素: " << poppedElement << endl;
 
-    cout << stack.GetTop() << endl;
-    if (stack.Empty()) {
-        cout << "栈为空" << endl;
-    } else {
-        cout << "栈非空" << endl;
-    }
+    // 判空操作
+    cout << "栈0是否为空: " << (stack.Empty(0) ? "是" : "否") << endl;
+    cout << "栈1是否为空: " << (stack.Empty(1) ? "是" : "否") << endl;
 
     return 0;
 }
+
